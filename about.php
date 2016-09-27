bf432d696f341879b66d30d078e677e2eb2b614a
Dean Andrew Hidas <dhidas@130-199-222-134.dhcp.bnl.gov>
2016-09-27 18:36:47 -0400

    add about

---
+++ about.php
@@ -0,0 +1,18 @@
+<?php include("includes/header.html"); ?>
+
+
+
+<h3>About</h3>
+
+
+<p>
+OSCARS began on the fine rails of the Long Island Rail Road, where nearly all of the core code was developed.  It was purely a project based on the curiosity of these calculations in 3D and presenting them in a coherent way, both to the user, and to the developer.
+</p>
+
+
+
+
+
+
+
+<?php include("includes/footer.html"); ?>
