862f27a47cfdf89ce4dd2ab99c3972bbc2955185
Dean Andrew Hidas <dhidas@130-199-222-134.dhcp.bnl.gov>
2016-12-01 12:22:01 -0500

    examples update

--- examples.php
+++ examples.php
@@ -2,8 +2,252 @@
 
 <h3>Examples</h3>
 <p>
-Here are a few examples of what one can easily do with OSCARS.
+Here you will find from simple to very detailed examples of calculations one can do using OSCARS.  The examples are followed by an "All About" section which describes in detail what one can do with specific features in OSCARS.
 </p>
+
+
+
+<p align="center">
+<img src="img/Flux_dipole.png" alt="Flux from dipole example" /><br />
+</p>
+
+
+<h4>Getting Started</h4>
+<p>
+These examples contain the most basics elements of OSCARS to get started quickly.
+</p>
+<table class="left">
+  <tr>
+    <td width="300">Undulator Spectrum</td>
+    <td><a href="examples/GettingStarted_UndulatorSpectrum.html">.html</a></td>
+    <td><a href="examples/GettingStarted_UndulatorSpectrum.ipynb">.ipynb</a></td>
+    <td><a href="examples/GettingStarted_UndulatorSpectrum.py">.py</a></td>
+  </tr>
+  <tr>
+    <td width="300">Undulator Flux</td>
+    <td><a href="examples/GettingStarted_UndulatorFlux.html">.html</a></td>
+    <td><a href="examples/GettingStarted_UndulatorFlux.ipynb">.ipynb</a></td>
+    <td><a href="examples/GettingStarted_UndulatorFlux.py">.py</a></td>
+  </tr>
+  <tr>
+    <td width="300">Undulator Power Density</td>
+    <td><a href="examples/GettingStarted_UndulatorPowerDensity.html">.html</a></td>
+    <td><a href="examples/GettingStarted_UndulatorPowerDensity.ipynb">.ipynb</a></td>
+    <td><a href="examples/GettingStarted_UndulatorPowerDensity.py">.py</a></td>
+  </tr>
+  <tr>
+    <td width="300">Undulator 3D Power Density</td>
+    <td><a href="examples/GettingStarted_Undulator3DPowerDensity.html">.html</a></td>
+    <td><a href="examples/GettingStarted_Undulator3DPowerDensity.ipynb">.ipynb</a></td>
+    <td><a href="examples/GettingStarted_Undulator3DPowerDensity.py">.py</a></td>
+  </tr>
+  <tr>
+    <td width="300">Importing Field Data</td>
+    <td><a href="examples/GettingStarted_ImportingFieldData.html">.html</a></td>
+    <td><a href="examples/GettingStarted_ImportingFieldData.ipynb">.ipynb</a></td>
+    <td><a href="examples/GettingStarted_ImportingFieldData.py">.py</a></td>
+  </tr>
+</table>
+
+
+
+
+<h4>Detailed Examples</h4>
+<p>
+These examples contain more realistic scenarios and explanations
+</p>
+<table class="left">
+  <tr>
+    <td width="300">001 Dipole Trajectory</td>
+    <td><a href="examples/Example_001_DipoleTrajectory.html">.html</a></td>
+    <td><a href="examples/Example_001_DipoleTrajectory.ipynb">.ipynb</a></td>
+    <td><a href="examples/Example_001_DipoleTrajectory.py">.py</a></td>
+    <td></td>
+  </tr>
+  <tr>
+    <td>002 Undulator Trajectory</td>
+    <td><a href="examples/Example_002_UndulatorTrajectory.html">.html</a></td>
+    <td><a href="examples/Example_002_UndulatorTrajectory.ipynb">.ipynb</a></td>
+    <td><a href="examples/Example_002_UndulatorTrajectory.py">.py</a></td>
+    <td></td>
+  </tr>
+  <tr>
+    <td>003 EPU Trajectory</td>
+    <td><a href="examples/Example_003_EPUTrajectory.html">.html</a></td>
+    <td><a href="examples/Example_003_EPUTrajectory.ipynb">.ipynb</a></td>
+    <td><a href="examples/Example_003_EPUTrajectory.py">.py</a></td>
+    <td></td>
+  </tr>
+  <tr>
+    <td>004 Undulator Plus BM Trajectory</td>
+    <td><a href="examples/Example_004_UndulatorPlusBMTrajectory.html">.html</a></td>
+    <td><a href="examples/Example_004_UndulatorPlusBMTrajectory.ipynb">.ipynb</a></td>
+    <td><a href="examples/Example_004_UndulatorPlusBMTrajectory.py">.py</a></td>
+    <td></td>
+  </tr>
+  <tr>
+    <td>005 Canted Straight Trajectory</td>
+    <td><a href="examples/Example_005_CantedStraightTrajectory.html">.html</a></td>
+    <td><a href="examples/Example_005_CantedStraightTrajectory.ipynb">.ipynb</a></td>
+    <td><a href="examples/Example_005_CantedStraightTrajectory.py">.py</a></td>
+    <td></td>
+  </tr>
+  <tr>
+    <td>006 Imported Field Trajectory</td>
+    <td><a href="examples/Example_006_ImportedFieldTrajectory.html">.html</a></td>
+    <td><a href="examples/Example_006_ImportedFieldTrajectory.ipynb">.ipynb</a></td>
+    <td><a href="examples/Example_006_ImportedFieldTrajectory.py">.py</a></td>
+    <td></td>
+  </tr>
+  <tr>
+    <td>022 Undulator Spectrum</td>
+    <td><a href="examples/Example_022_UndulatorSpectrum.html">.html</a></td>
+    <td><a href="examples/Example_022_UndulatorSpectrum.ipynb">.ipynb</a></td>
+    <td><a href="examples/Example_022_UndulatorSpectrum.py">.py</a></td>
+    <td></td>
+  </tr>
+  <tr>
+    <td>023 EPU Spectrum</td>
+    <td><a href="examples/Example_023_EPUSpectrum.html">.html</a></td>
+    <td><a href="examples/Example_023_EPUSpectrum.ipynb">.ipynb</a></td>
+    <td><a href="examples/Example_023_EPUSpectrum.py">.py</a></td>
+    <td></td>
+  </tr>
+  <tr>
+    <td>032 Undulator Flux</td>
+    <td><a href="examples/Example_032_UndulatorFlux.html">.html</a></td>
+    <td><a href="examples/Example_032_UndulatorFlux.ipynb">.ipynb</a></td>
+    <td><a href="examples/Example_032_UndulatorFlux.py">.py</a></td>
+    <td></td>
+  </tr>
+  <tr>
+    <td>033 EPU Flux</td>
+    <td><a href="examples/Example_033_EPUFlux.html">.html</a></td>
+    <td><a href="examples/Example_033_EPUFlux.ipynb">.ipynb</a></td>
+    <td><a href="examples/Example_033_EPUFlux.py">.py</a></td>
+    <td></td>
+  </tr>
+  <tr>
+    <td>042 Undulator Power Density</td>
+    <td><a href="examples/Example_042_UndulatorPowerDensity.html">.html</a></td>
+    <td><a href="examples/Example_042_UndulatorPowerDensity.ipynb">.ipynb</a></td>
+    <td><a href="examples/Example_042_UndulatorPowerDensity.py">.py</a></td>
+    <td></td>
+  </tr>
+  <tr>
+    <td>043 EPU Power Density</td>
+    <td><a href="examples/Example_043_EPUPowerDensity.html">.html</a></td>
+    <td><a href="examples/Example_043_EPUPowerDensity.ipynb">.ipynb</a></td>
+    <td><a href="examples/Example_043_EPUPowerDensity.py">.py</a></td>
+    <td></td>
+  </tr>
+  <tr>
+    <td>122 Undulator Spectrum Multi-Particle</td>
+    <td><a href="examples/Example_122_UndulatorSpectrum_MultiParticle.html">.html</a></td>
+    <td><a href="examples/Example_122_UndulatorSpectrum_MultiParticle.ipynb">.ipynb</a></td>
+    <td><a href="examples/Example_122_UndulatorSpectrum_MultiParticle.py">.py</a></td>
+    <td><a href="examples/Example_122_UndulatorSpectrum_MultiParticle_MPI.py">MPI</a></td>
+  </tr>
+  <tr>
+    <td>123 EPU Spectrum Multi-Particle</td>
+    <td><a href="examples/Example_123_EPUSpectrum_MultiParticle.html">.html</a></td>
+    <td><a href="examples/Example_123_EPUSpectrum_MultiParticle.ipynb">.ipynb</a></td>
+    <td><a href="examples/Example_123_EPUSpectrum_MultiParticle.py">.py</a></td>
+    <td><a href="examples/Example_123_EPUSpectrum_MultiParticle_MPI.py">MPI</a></td>
+  </tr>
+  <tr>
+    <td>132 Undulator Flux Multi-Particle</td>
+    <td><a href="examples/Example_132_UndulatorFlux_MultiParticle.html">.html</a></td>
+    <td><a href="examples/Example_132_UndulatorFlux_MultiParticle.ipynb">.ipynb</a></td>
+    <td><a href="examples/Example_132_UndulatorFlux_MultiParticle.py">.py</a></td>
+    <td><a href="examples/Example_132_UndulatorFlux_MultiParticle_MPI.py">MPI</a></td>
+  </tr>
+  <tr>
+    <td>133 EPU Flux Multi-Particle</td>
+    <td><a href="examples/Example_133_EPUFlux_MultiParticle.html">.html</a></td>
+    <td><a href="examples/Example_133_EPUFlux_MultiParticle.ipynb">.ipynb</a></td>
+    <td><a href="examples/Example_133_EPUFlux_MultiParticle.py">.py</a></td>
+    <td><a href="examples/Example_133_EPUFlux_MultiParticle_MPI.py">MPI</a></td>
+  </tr>
+  <tr>
+    <td>142 Undulator Power Density Multi-Particle</td>
+    <td><a href="examples/Example_142_UndulatorPowerDensity_MultiParticle.html">.html</a></td>
+    <td><a href="examples/Example_142_UndulatorPowerDensity_MultiParticle.ipynb">.ipynb</a></td>
+    <td><a href="examples/Example_142_UndulatorPowerDensity_MultiParticle.py">.py</a></td>
+    <td><a href="examples/Example_142_UndulatorPowerDensity_MultiParticle_MPI.py">MPI</a></td>
+  </tr>
+  <tr>
+    <td>143 EPU Power Density Multi-Particle</td>
+    <td><a href="examples/Example_143_EPUPowerDensity_MultiParticle.html">.html</a></td>
+    <td><a href="examples/Example_143_EPUPowerDensity_MultiParticle.ipynb">.ipynb</a></td>
+    <td><a href="examples/Example_143_EPUPowerDensity_MultiParticle.py">.py</a></td>
+    <td><a href="examples/Example_143_EPUPowerDensity_MultiParticle_MPI.py">MPI</a></td>
+  </tr>
+  <tr>
+    <td>342 Undulator Power Density 3D Sphere</td>
+    <td><a href="examples/Example_342_UndulatorPowerDensity_Sphere.html">.html</a></td>
+    <td><a href="examples/Example_342_UndulatorPowerDensity_Sphere.ipynb">.ipynb</a></td>
+    <td><a href="examples/Example_342_UndulatorPowerDensity_Sphere.py">.py</a></td>
+    <td></td>
+  </tr>
+</table>
+
+
+
+
+
+
+
+
+<h4>All About</h4>
+<p>
+These examples contain details on the objects used in OSCARS.
+</p>
+<table class="left">
+  <tr>
+    <td width="300">Magnetic Fields</td>
+    <td><a href="examples/AllAbout_MagneticFields.html">.html</a></td>
+    <td><a href="examples/AllAbout_MagneticFields.ipynb">.ipynb</a></td>
+  </tr>
+  <tr>
+    <td>Electric Fields</td>
+    <td><a href="examples/AllAbout_ElectricFields.html">.html</a></td>
+    <td><a href="examples/AllAbout_ElectricFields.ipynb">.ipynb</a></td>
+  </tr>
+  <tr>
+    <td>Particle Beams</td>
+    <td><a href="examples/AllAbout_ParticleBeams.html">.html</a></td>
+    <td><a href="examples/AllAbout_ParticleBeams.ipynb">.ipynb</a></td>
+  </tr>
+  <tr>
+    <td>Flux</td>
+    <td><a href="examples/AllAbout_Flux.html">.html</a></td>
+    <td><a href="examples/AllAbout_Flux.ipynb">.ipynb</a></td>
+  </tr>
+  <tr>
+    <td>Spectra</td>
+    <td><a href="examples/AllAbout_Spectra.html">.html</a></td>
+    <td><a href="examples/AllAbout_Spectra.ipynb">.ipynb</a></td>
+  </tr>
+  <tr>
+    <td>PowerDensity</td>
+    <td><a href="examples/AllAbout_PowerDensity.html">.html</a></td>
+    <td><a href="examples/AllAbout_PowerDensity.ipynb">.ipynb</a></td>
+  </tr>
+  <tr>
+    <td>GPUAndThreads</td>
+    <td><a href="examples/AllAbout_GPUAndThreads.html">.html</a></td>
+    <td><a href="examples/AllAbout_GPUAndThreads.ipynb">.ipynb</a></td>
+  </tr>
+  <tr>
+    <td>ParametricSurfaces</td>
+    <td><a href="examples/AllAbout_ParametricSurfaces.html">.html</a></td>
+    <td><a href="examples/AllAbout_ParametricSurfaces.ipynb">.ipynb</a></td>
+  </tr>
+</table>
+
+
+
 
 
 <h4>Spectra</h4>
@@ -11,11 +255,6 @@ Here are a few examples of what one can easily do with OSCARS.
 <img src="img/Spectrum1.png" /><img src="img/Spectrum2.png" />
 </p>
 
-<h4>Flux Density</h4>
-<p align="center">
-<img src="img/Flux_dipole.png" alt="Flux from dipole example" /><br />
-Example flux calculation 400 x 2000 pixels on 2015 mackbook in &lt; 60s
-</p>
 
 <h4>Power Density on Arbitrary Shaped Objects in 3D</h4>
 <p align="center">
@@ -24,22 +263,6 @@ Example flux calculation 400 x 2000 pixels on 2015 mackbook in &lt; 60s
 
 
 
-
-<table class="left" width="300">
-  <tr>
-    <td colspan=3><b><u>All About</u></b></td>
-  </tr>
-  <tr>
-    <td width="50%">Magnetic Fields</td>
-    <td width="25%"><a href="examples/AllAbout_MagneticFields.html">.html</a></td>
-    <td width="25%"><a href="examples/AllAbout_MagneticFields.ipynb">.ipynb</a></td>
-  </tr>
-  <tr>
-    <td width="50%">Particle Beams</td>
-    <td width="25%"><a href="examples/AllAbout_ParticleBeams.html">.html</a></td>
-    <td width="25%"><a href="examples/AllAbout_ParticleBeams.ipynb">.ipynb</a></td>
-  </tr>
-</table>
 
 
 
