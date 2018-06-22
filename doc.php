2de0e2c4612c22b2f49e50178e44c96dd453119f
Hidas <dhidas@dhidas.com>
2018-06-22 16:20:44 -0400

    w3c update

--- doc.php
+++ doc.php
@@ -12,7 +12,7 @@ For documentation on installation please see the <a href="download.php">download
 <table class="left" width="300">
   <tr>
     <td width="50%"><b>Current release</b></td>
-    <td width="40%"><a target="_blank" href="doc/2.1.2">2.1.2</a></td><td width="10%">2018.06.22</td>
+    <td width="40%"><a href="doc/2.1.2">2.1.2</a></td><td width="10%">2018.06.22</td>
   </tr>
   <tr>
     <td>&nbsp;</td>
@@ -20,43 +20,43 @@ For documentation on installation please see the <a href="download.php">download
   </tr>
   <tr>
     <td><b>Past releases</b></td>
-    <td><a target="_blank" href="doc/2.1.0">2.1.0</a></td><td>2018.05.01</td>
+    <td><a href="doc/2.1.0">2.1.0</a></td><td>2018.05.01</td>
   </tr>
   <tr>
     <td><b></b></td>
-    <td><a target="_blank" href="doc/2.00.xx">2.0.20</a></td><td>2018.02.20</td>
+    <td><a href="doc/2.00.xx">2.0.20</a></td><td>2018.02.20</td>
   </tr>
   <tr>
     <td></td>
-    <td><a target="_blank" href="doc/2.00.xx">2.0.12</a></td><td>2018.01.19</td>
+    <td><a href="doc/2.00.xx">2.0.12</a></td><td>2018.01.19</td>
   </tr>
   <tr>
     <td></td>
-    <td><a target="_blank" href="doc/2.00.xx">2.0.6</a></td><td>2017.12.04</td>
+    <td><a href="doc/2.00.xx">2.0.6</a></td><td>2017.12.04</td>
   </tr>
   <tr>
     <td></td>
-    <td><a target="_blank" href="doc/2.00.xx">2.0.4</a></td><td>2017.10.27</td>
+    <td><a href="doc/2.00.xx">2.0.4</a></td><td>2017.10.27</td>
   </tr>
   <tr>
     <td></td>
-    <td><a target="_blank" href="doc/2.00.xx">2.00.02</a></td><td>2017.10.11</td>
+    <td><a href="doc/2.00.xx">2.00.02</a></td><td>2017.10.11</td>
   </tr>
   <tr>
     <td></td>
-    <td><a target="_blank" href="doc/1.36.xx">1.36.14</a></td><td>2017.09.12</td>
+    <td><a href="doc/1.36.xx">1.36.14</a></td><td>2017.09.12</td>
   </tr>
   <tr>
     <td><b></b></td>
-    <td><a target="_blank" href="doc/1.36.12">1.36.12</a></td><td>2017.05.18</td>
+    <td><a href="doc/1.36.12">1.36.12</a></td><td>2017.05.18</td>
   </tr>
   <tr>
     <td></td>
-    <td><a target="_blank" href="doc/1.34.00">1.34.00</a></td><td>2017.01.05</td>
+    <td><a href="doc/1.34.00">1.34.00</a></td><td>2017.01.05</td>
   </tr>
   <tr>
     <td></td>
-    <td><a target="_blank" href="doc/1.32.00">1.32.00</a></td><td>2016.12.01</td>
+    <td><a href="doc/1.32.00">1.32.00</a></td><td>2016.12.01</td>
   </tr>
 </table>
 
